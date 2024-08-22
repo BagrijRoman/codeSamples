b31caccf907294eb79ba9569827910f445db951e
BagrijRoman <bagrijroman@gmail.com>
2024-08-22 19:15:36 +0300

    add settings bar inputs

--- canvas_websocket_multiuser/client/src/components/toolbar/SettingsBar.tsx
+++ canvas_websocket_multiuser/client/src/components/toolbar/SettingsBar.tsx
@@ -1,12 +1,18 @@
 import React from 'react';
 
-
 import './toolbar.less';
 
 export const SettingsBar = () => {
   return (
     <div className="settings-bar toolbar">
-
+      <div className='settings-bar-input-wrapper'>
+        <span className="settings-bar-label">Line thickness: </span>
+        <input type='number' min='1' max='50'/>
+      </div>
+      <div className='settings-bar-input-wrapper'>
+        <span className="settings-bar-label">Border color: </span>
+        <input type="color" className="button btn-color"/>
+      </div>
     </div>
   );
 };
