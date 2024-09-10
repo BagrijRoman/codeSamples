8199cab8522d9448178048e704ddf0c4e6bd80a6
BagrijRoman <bagrijroman@gmail.com>
2024-09-10 20:45:06 +0300

    add router with identifer generation

--- canvas_websocket_multiuser/client/src/App.tsx
+++ canvas_websocket_multiuser/client/src/App.tsx
@@ -1,4 +1,5 @@
 import React from 'react';
+import { BrowserRouter, Routes, Route, Navigate } from 'react-router-dom';
 
 import { Canvas, Toolbar, SettingsBar } from './components';
 
@@ -7,10 +8,23 @@ import './styles/app.less';
 export const App = () => {
 
   return (
-    <div>
-      <Toolbar/>
-      <SettingsBar/>
-      <Canvas/>
-    </div>
+    <BrowserRouter>
+      <Routes>
+        <Route
+          path="/:id"
+          element={
+            <div>
+              <Toolbar/>
+              <SettingsBar/>
+              <Canvas/>
+            </div>
+          }
+        />
+        <Route
+          path="*"
+          element={ <Navigate to={ `/f${ (+new Date).toString(16)}`} replace />}
+        />
+      </Routes>
+    </BrowserRouter>
   );
 };
--- canvas_websocket_multiuser/server/src/index.ts
+++ canvas_websocket_multiuser/server/src/index.ts
@@ -1,12 +1,58 @@
 import express from 'express';
+import expressWs from 'express-ws';
+import { WebSocket } from 'ws';
 
+// !!!!!!!!  todo decompose file
 
-const app = express();
-
+const { app, getWss, applyTo } = expressWs(express());
+const aWss = getWss();
 const PORT = process.env.PORT || 5005;
 
+
+interface ClientWebSocket extends WebSocket {
+  id?: string;
+}
+
 app.listen(PORT, () => {
-  console.log(`Listening on port ${PORT}`);
+  console.log(`Listening on port ${ PORT }`);
 });
 
 app.all('/test', (req: express.Request, res: express.Response) => res.json({ serverStatus: "Ok" }))
+
+
+app.ws('/', (ws: ClientWebSocket, req) => {
+  console.log('new ws connection was established');
+  ws.send('You was successfully connected');
+
+  ws.on('message', (message: string) => {
+    const msg = JSON.parse(message);
+
+    switch (msg.method) {
+      case 'connection': {
+        connectionHandler(ws, message);
+        break;
+      }
+
+      default: {
+        break;
+      }
+    }
+  });
+});
+
+
+const connectionHandler = (ws: ClientWebSocket, message: string) => {
+  const msg = JSON.parse(message);
+  ws.id = msg.id;
+  broadcastConnection(ws, message);
+}
+
+const broadcastConnection = (ws: ClientWebSocket, message: string) => {
+  const msg = JSON.parse(message);
+  aWss.clients.forEach((client: ClientWebSocket) => {
+    if (client.id === msg.id) {
+      client.send(`User: ${msg.userName} was connected`);
+    }
+  })
+}
+
